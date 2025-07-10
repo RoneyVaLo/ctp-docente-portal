f5280e9b53ea4422c51ff32e02a844007a339f60
RoneyVaLo <108905119+RoneyVaLo@users.noreply.github.com>
2025-07-09 18:53:02 -0600

    Create DATA for student evaluation
    DTOs were created for the student evaluations.

---
+++ ctp-docente-portal.Server/DTOs/StudentEvaluationScores/StudentEvaluationScoreCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ctp_docente_portal.Server.DTOs.StudentEvaluationScores
+{
+    public class StudentEvaluationScoreCreateDto
+    {
+        public int StudentId { get; set; }
+        public int EvaluationCriteriaId { get; set; }
+        public decimal Score { get; set; }
+    }
+}
