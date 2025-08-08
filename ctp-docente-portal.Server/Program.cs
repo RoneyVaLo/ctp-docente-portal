7476779dbcd2f8790896b2fd963874646aa3aaa3
Josafath17 <JOSA17102@GMAIL.COM>
2025-08-08 00:05:23 -0600

    Everything works now from the program
    Fixed program version correction

--- ctp-docente-portal.Server/Program.cs
+++ ctp-docente-portal.Server/Program.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using ctp_docente_portal.Server.Data;
 using ctp_docente_portal.Server.Mappings;
+//using ctp_docente_portal.Server.Middleware;
 using ctp_docente_portal.Server.Services.Implementations;
-using ctp_docente_portal.Server.Services.Implentations;
 using ctp_docente_portal.Server.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -18,7 +18,7 @@ namespace ctp_docente_portal.Server
             // Add DbContext with PostgreSQL
 
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
+            
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseNpgsql(connectionString)
                 .LogTo(Console.WriteLine, LogLevel.Information));
@@ -28,34 +28,35 @@ namespace ctp_docente_portal.Server
             configExpr.AddProfile<MappingProfile>();
 
             // 2) Pasa un ILoggerFactory (aquí usamos NullLoggerFactory para no depender del logging real)
-            var mapperConfig = new MapperConfiguration(
-                configExpr,
-                NullLoggerFactory.Instance
-            );
+                var mapperConfig = new MapperConfiguration(
+                    configExpr,
+                    NullLoggerFactory.Instance
+                );
 
             // 3) Crea el IMapper y regístralo
             var mapper = mapperConfig.CreateMapper();
             builder.Services.AddSingleton<IMapper>(mapper);
 
 
-            // Add services to the container.
-            builder.Services.AddScoped<IWhatsAppApiService, WhatsAppApiService>();
-            builder.Services.AddScoped<IReportService, ReportService>();
-            app.UseMiddleware<ctp_docente_portal.Server.Middlewares.RoleAuthorizationMiddleware>();
-
-
-
-
             builder.Services.AddControllers();
 
             // TODO: Así se deben añadir todas las Interfaces (INTERFACE) y sus Implementaciones (SERVICES)
-            builder.Services.AddScoped<IEvaluationCriteriaService, EvaluationCriteriaService>();
-            builder.Services.AddScoped<ISubjectEvaluationService, SubjectEvaluationService>();
+            // builder.Services.AddScoped<IEvaluationCategoriesService, EvaluationCategoriesService>();
+             //builder.Services.AddScoped<IEvaluationCriteriaService, EvaluationCriteriaService>();
+             //builder.Services.AddScoped<IEvaluationItemService, EvaluationItemService>();
+             //builder.Services.AddScoped<IAcademicPeriodService, AcademicPeriodService>();
+             //builder.Services.AddScoped<ISectionService, SectionService>();
+             //builder.Services.AddScoped<ISubjectService, SubjectService>();
+             //builder.Services.AddScoped<IStudentService, StudentService>();
+             //builder.Services.AddScoped<IEvaluationScoreService, EvaluationScoreService>();
 
             var app = builder.Build();
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+
+            // Middleware to management errors
+             //app.UseMiddleware<ErrorHandlingMiddleware>();
 
             // Configure the HTTP request pipeline.
 
@@ -67,7 +68,6 @@ namespace ctp_docente_portal.Server
             app.MapControllers();
 
             app.MapFallbackToFile("/index.html");
-            builder.Services.AddScoped<IAttendanceService, AttendanceService>();
 
             app.Run();
         }
